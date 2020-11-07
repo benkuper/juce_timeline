435950e01da13cd3232f5975f95e6d21650717ac
Ben Kuper <bkuperberg@gmail.com>
2020-11-07 09:56:57 +0100

    fix play button > togglePlay

--- timeline/Sequence/ui/SequenceTransportUI.h
+++ timeline/Sequence/ui/SequenceTransportUI.h
@@ -11,24 +11,29 @@
 #pragma once
 
 class SequenceTransportUI :
-	public Component
+	public Component,
+	public Sequence::AsyncListener
 {
 public:
 	SequenceTransportUI(Sequence * _sequence);
 	virtual ~SequenceTransportUI();
 
 	Sequence * sequence;
+	WeakReference<Inspectable> sequenceRef;
+
 	TimeLabel timeLabel;
 	TimeLabel timeStepLabel;
 	TimeLabel totalTimeLabel;
-	
-	std::unique_ptr<BoolImageToggleUI> togglePlayUI;
+
+	std::unique_ptr<TriggerImageUI> togglePlayUI;
 	std::unique_ptr<TriggerImageUI> stopUI;
 	std::unique_ptr<TriggerImageUI> prevCueUI;
 	std::unique_ptr<TriggerImageUI> nextCueUI;
 	std::unique_ptr<BoolImageToggleUI> loopUI;
 	
 	void resized() override;
+
+	void newMessage(const Sequence::SequenceEvent & e) override;
 
 	JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(SequenceTransportUI)
 };
